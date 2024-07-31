750723297c8fd1afc4a254c1cb7091e7ec8a9b70
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-07-31 22:41:26 +0700

    add: Hangman Game

---
+++ hangmanGame/main.py
@@ -0,0 +1,34 @@
+import os
+from player import Player
+
+player = Player(5)
+
+message = ""
+while True:
+    os.system('cls')  # Clear Screen
+
+    player.get_info()
+    print()  # Extra space
+    print(message)
+    message = ""
+
+    user_input = input("Please insert a one character (type \\exit to exit) : ")
+    if user_input == "\\exit":
+        print("Well Played, Goodbye...")
+        exit(0)
+
+    if len(user_input) > 1 or len(user_input) == 0:
+        message = "Invalid Input...\n"
+        continue
+
+    player.check_character(user_input.lower())
+
+    if player.chance == 0:
+        player.get_info()
+        print("\nYou Lose")
+        break
+
+    if player.is_win():
+        player.get_info()
+        print("\nCongratulation, You win...")
+        break
---
+++ hangmanGame/player.py
@@ -0,0 +1,37 @@
+import random
+
+
+class Player:
+    __w = ("mouse", "keyboard", "monitor", "tumbler", "motorcycle", "airplane", "computer", "cardboard")
+    __words = ""
+    result = []
+
+    def __init__(self, chance):
+        self.chance = chance
+        self.__generate_random_words()
+
+    def __generate_random_words(self):
+        self.__words = random.choice(self.__w)
+
+        for _ in range(len(self.__words)):
+            self.result.append("-")
+
+    def check_character(self, char):
+        is_found = False
+        for i in range(len(self.__words)):
+            if self.__words[i] == char:
+                self.result[i] = char
+                is_found = True
+
+        if not is_found:
+            self.chance -= 1
+
+    def is_win(self):
+        return True if self.__words == ''.join(self.result) else False
+
+    def get_info(self):
+        result = ' '.join(self.result)
+
+        print("*** Status ***")
+        print(f"Chance: {self.chance}")
+        print(f"Result: {result}")
